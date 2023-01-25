05303665f81c97e8f24e0ff6aab8eb94a11f737c
SereneMatt <serene1990@gmail.com>
2023-01-24 22:46:08 -0600

    Initial Commit

---
+++ WhoIsMatt/WhoIsMattApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoIsMattApp.swift
+//  WhoIsMatt
+//
+//  Created by Serene Mathew on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WhoIsMattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
